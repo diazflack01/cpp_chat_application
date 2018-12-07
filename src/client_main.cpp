0c5dea2fcf851bfed2f8fe7c629e2fe728d28651
Kelvin Robles <kelvin.robles018@gmail.com>
2018-12-07 17:00:10 +0800

    Added components in chat UI w/ basic signal to slot connection

--- src/client_main.cpp
+++ src/client_main.cpp
@@ -1,18 +1,56 @@
 #include <iostream>
 
+#include <QApplication>
+#include <QMainWindow>
+
 #include "ui_mainwindow.h"
+#include "ui_form.h"
 #include "client.h"
+
+class MaWindow : public QMainWindow
+{
+    Q_OBJECT
+
+    Ui::MainWindow* ui;
+
+private slots:
+    void on_pushButton_clicked()
+    {
+        ui->plainTextEdit->setPlainText(ui->plainTextEdit_2->toPlainText());
+        ui->plainTextEdit_2->clear();
+    }
+public:
+    explicit MaWindow(QMainWindow* parent = nullptr)
+        : QMainWindow(parent)
+        , ui(new Ui::MainWindow)
+    {
+        ui->setupUi(this);
+    }
+
+    ~MaWindow()
+    {
+        delete ui;
+    }
+};
+
+#include "client_main.moc"
 
 int main(int argc, char** argv)
 {
-    std::cout << "main\n";
-    Client c;
-    c.connectTo(argv[1], atoi(argv[2]));
-    if(!c.isConnected())
-    {
-        std::cout << "Unable to connect to host\n";
-    }
-    c.startCommunication();
+    QApplication app(argc, argv);
 
-    return 0;
+    std::cout << "main\n";
+
+    MaWindow m;
+    m.show();
+
+//    Client c;
+//    c.connectTo(argv[1], atoi(argv[2]));
+//    if(!c.isConnected())
+//    {
+//        std::cout << "Unable to connect to host\n";
+//    }
+//    c.startCommunication();
+
+    return app.exec();
 }
